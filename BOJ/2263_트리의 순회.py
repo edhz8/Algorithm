86a27817c424c5e01dc80ea23a0917c840491287
edhz8888 <edhz8888@gmail.com>
2021-10-01 00:07:14 +0900

    10172_개

---
+++ BOJ/2263_트리의 순회.py
@@ -0,0 +1,18 @@
+import sys
+I = sys.stdin.readline
+n=int(I())
+graph,graph[1] =[0 for _ in range(n+1)],-1
+for _ in range(n-1):
+    a,b = map(int,I().split())
+    if a>b: a,b=b,a
+    if graph[a]: graph[b]=a
+    else : graph[a]=b
+for _ in range(int(I())):
+    a,b = map(int,I().split())
+    a_ans=[a]
+    while a != 1:
+        a=graph[a]
+        a_ans.append(a)
+    while True:
+        if b in a_ans : print(b);break
+        else : b=graph[b]
