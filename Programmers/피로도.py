07584fbc3fbf3478e702b5037c0d089329cef979
edhz8888 <edhz8888@gmail.com>
2021-10-25 21:18:34 +0900

    피로도

---
+++ Programmers/피로도.py
@@ -0,0 +1,11 @@
+from collections import deque
+def solution(k, dungeons):
+    L,n,dp = len(dungeons),0,deque()
+    dp.append([k,[]])
+    while dp:
+        p,v=dp.popleft()
+        for i in range(L):
+            [a,b]=dungeons[i]
+            if i not in v and p>=a and p-b>=1: dp.append([p-b,v+[i]])
+            else : n=max(n,len(v))
+    return n
