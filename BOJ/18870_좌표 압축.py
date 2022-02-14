fa620f4195686c5ce647c6351fba19bfe9e8fd7e
edhz8888 <edhz8888@gmail.com>
2022-02-14 17:44:15 +0900

    boj 18870

--- BOJ/18870_좌표 압축.py
+++ BOJ/18870_좌표 압축.py
@@ -1,10 +1,7 @@
-import sys
+import sys 
 input = sys.stdin.readline
-N=int(input())
-nums,snums=[],set()
-for i in input().split():
-    i=int(i)
-    nums.append(i)
-    snums.add(i)
-s=sorted(snums)
-print(*[s.index(n) for n in nums])
+N = int(input()) 
+l = list(map(int,input().split())) 
+l2 = list(sorted(set(l))) 
+dic = {l2[i]:i for i in range (len(l2))} 
+print(*[dic[n] for n in l])
