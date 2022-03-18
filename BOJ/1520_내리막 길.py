4347ebee46117c8424491a29bd4aa7e18bf4e844
edhz8888 <edhz8888@gmail.com>
2022-03-18 22:27:42 +0900

    boj 1520

--- BOJ/1520_내리막 길.py
+++ BOJ/1520_내리막 길.py
@@ -1,15 +1,14 @@
 import sys
-input = sys.stdin.readline
+I = sys.stdin.readline
 sys.setrecursionlimit(10**6)
-n,m = map(int,input().split())
-graph,dp = [list(map(int,input().split())) for _ in range(n)],[[-1 for _ in range(m)] for _ in range(n)]
-D = [(-1,0),(0,1),(1,0),(0,-1)]
+n,m = map(int,I().split())
+g,dp = [list(map(int,I().split())) for _ in range(n)],[[-1 for _ in range(m)] for _ in range(n)]
 def dfs(x,y):
     if x==n-1 and y==m-1 : return 1
-    if dp[x][y] != -1 : return dp[x][y]
+    if dp[x][y] > -1 : return dp[x][y]
     dp[x][y] = 0
-    for dx,dy in D:
+    for dx,dy in [(-1,0),(0,1),(1,0),(0,-1)]:
         nx,ny = x+dx,y+dy
-        if 0<=nx<n and 0<=ny<m and  graph[nx][ny] < graph[x][y]: dp[x][y] += dfs(nx,ny)
+        if 0<=nx<n and 0<=ny<m and  g[nx][ny] < g[x][y]: dp[x][y] += dfs(nx,ny)
     return dp[x][y]
 print(dfs(0,0))
