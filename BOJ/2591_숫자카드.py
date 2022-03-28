49c2f8f669bcab343c5527a07747c1b201bbc6c7
edhz8888 <edhz8888@gmail.com>
2022-03-28 23:42:37 +0900

    boj 2591

--- BOJ/2591_숫자카드.py
+++ BOJ/2591_숫자카드.py
@@ -1,6 +1,6 @@
-S=input()
-L=len(S)
-dp,dp[1],dp[2]=[0 for _ in range(L+1)],1,(1 if int(S[:2]) > 34 else 2)
-for i in range(3,L+1):
-    dp[i] = (dp[i-1] if int(S[i-1]) > 0 else 0) + (dp[i-2] if 10 <= int(S[i-2:i]) <= 34 else 0)
+s = input()
+L,dp = len(s),[0 for _ in range(len(s))]
+dp[0] = 1
+dp[1] = (dp[0] if int(s[1])>0 else 0) + (9<int(s[:2])<35)
+for i in range(2,L): dp[i] += (dp[i-1] if int(s[i])>0 else 0) + (dp[i-2] if 9<int(s[i-1:i+1])<35 else 0)
 print(dp[-1])
