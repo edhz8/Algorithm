439e4a7ad61dc9277edd680a85880ba280cd2321
GyuhyunBaek <edhz8888@gmail.com>
2022-05-11 14:41:47 +0900

    boj 1501

---
+++ BOJ/1501_영어 읽기.py
@@ -0,0 +1,17 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+dic = defaultdict(dict)
+for _ in range(int(input())):
+    line = input().strip()
+    i = line[0]+line[-1] if len(line)>1 else line[0]
+    k = ''.join(sorted(line[1:-1]))
+    dic[i][k] = dic[i].get(k,0)+1
+for _ in range(int(input())):
+    ans = 1
+    for line in input().strip().split():
+        i = line[0]+line[-1] if len(line)>1 else line[0]
+        k = ''.join(sorted(line[1:-1]))
+        ans *= dic[i].get(k,0)
+    print(ans)
+print(dic)
