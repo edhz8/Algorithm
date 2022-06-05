f62b00f1d47391fb1377633a1d0f25ee13423118
GyuhyunBaek <edhz8888@gmail.com>
2022-06-05 12:22:15 +0900

    boj 1004

---
+++ BOJ/1004_어린 왕자.py
@@ -0,0 +1,9 @@
+for _ in ' '*int(input()):
+    ans=0
+    x1,y1,x2,y2=map(int,input().split())
+    for _ in ' '*int(input()):
+        px,py,pr=map(int,input().split())
+        d1 = (((x1-px)**2)+((y1-py)**2))**0.5
+        d2 = (((x2-px)**2)+((y2-py)**2))**0.5
+        if (d1<pr and d2>pr) or (d1>pr and d2<pr):ans+=1
+    print(ans)
