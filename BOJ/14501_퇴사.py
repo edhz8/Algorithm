f259826b43282bbd234d08fd60140f03b48cabb2
GyuhyunBaek <edhz8888@gmail.com>
2023-01-08 22:52:58 +0900

    boj 14501

---
+++ BOJ/14501_퇴사.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+N=int(input())
+dp=[0]*N
+answer,prev=0,0
+for i in range(N):
+    t,p=map(int,input().split())
+    prev=max(prev,dp[i-1] if i!=0 else 0)
+    if i+t-1<N: 
+        dp[i+t-1] = max(dp[i+t-1],prev + p)
+        answer = max(answer,dp[i+t-1])
+print(answer)
