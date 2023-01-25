092b783c6d7809d703998e28b43c19ec81e8a6be
GyuhyunBaek <edhz8888@gmail.com>
2023-01-25 14:34:59 +0900

    boj 12919

--- BOJ/12919_A와 B 2.py
+++ BOJ/12919_A와 B 2.py
@@ -1,11 +1,8 @@
-from abc import ABC
-
-
-def rec(s):
-    if s == S : print(1);exit(0)
-    if len(s) == len(S) : return
-    if s[-1]=='A' : rec(s[:-1])
-    if s[0]=='B' : rec(s[1:][::-1])
-S,T=input(),input()
-rec(T)
+S=input()
+def R(t):
+    if t==S:print(1);exit(0)
+    if len(t)==len(S):return
+    if t[0]=='B':R(t[:0:-1])
+    if t[-1]=='A':R(t[:-1])
+R(input())
 print(0)
