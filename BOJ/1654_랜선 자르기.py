1982528d722de4a18f9e02cafc136d664d73d4f5
GyuhyunBaek <edhz8888@gmail.com>
2023-02-05 14:02:02 +0900

    boj 1654

--- BOJ/1654_랜선 자르기.py
+++ BOJ/1654_랜선 자르기.py
@@ -1,12 +1,10 @@
-K,N = map(int,input().split())
-ks,L = [int(input()) for _ in range(K)],0
-def SUM(n): return sum([k//n for k in ks])
-while 1:
-    po = 1
-    while 1:
-        if  SUM(L+po)>=N: po *=2
-        else : 
-            po//=2
-            if N > SUM(L+po+1): print(L+po);exit(0)
-            break
-    L += po
+import sys
+input = sys.stdin.readline
+K,N=map(int,input().split())
+ks=[int(input()) for _ in ' '*K]
+s,e = 1,max(ks)
+while s<=e:
+    m = (s+e)//2
+    if sum(k//m for k in ks)>=N : s=m+1
+    else: e=m-1
+print(e)
