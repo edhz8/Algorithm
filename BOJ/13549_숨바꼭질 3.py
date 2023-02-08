3b5ddf0655fa58b404be7ba33cecee990707a4af
GyuhyunBaek <edhz8888@gmail.com>
2023-02-08 12:08:07 +0900

    boj 13549

--- BOJ/13549_숨바꼭질 3.py
+++ BOJ/13549_숨바꼭질 3.py
@@ -1,14 +1,15 @@
-from heapq import heappop,heappush
+from heapq import heappush,heappop
 N,K=map(int,input().split())
-q=[]
-L = 100001
-dp,dp[N] = [float('inf') for _ in range(L)],0
+L=100000
+dp,q=[float('inf')]*(L+1),[]
+dp[N]=0
 heappush(q,(0,N))
 while q:
-    time,index = heappop(q)
-    for node in[(0,index*2),(1,index+1),(1,index-1)]:
-        if node[1] <0 or node[1]>=L : continue
-        if node[0]+dp[index] < dp[node[1]]:
-            dp[node[1]] = node[0]+dp[index]
-            heappush(q,node)
+    time,index=heappop(q)
+    if index==K:break
+    for t,i in [(0,index*2),(1,index+1),(1,index-1)]:
+        if not 0<=i<=L: continue
+        if t+dp[index] < dp[i]:
+            dp[i] = t+dp[index]
+            heappush(q,(t+dp[index],i))
 print(dp[K])
