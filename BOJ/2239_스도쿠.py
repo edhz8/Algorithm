7308079e6cd19ce973d98c8bdd8befe0c888040a
GyuhyunBaek <edhz8888@gmail.com>
2023-02-10 14:52:44 +0900

    boj 2239

---
+++ BOJ/2239_스도쿠.py
@@ -0,0 +1,18 @@
+G=[[int(n) for n in input()] for _ in '9'*9]
+ijs=[(i,j) for i in range(9) for j in range(9) if not G[i][j]]
+def rec(n):
+    if n==len(ijs):
+        for num in range(9):print(*G[num],sep='')
+        exit(0)
+    i,j=ijs[n]
+    nums = [0]*10
+    for k in range(9): nums[G[i][k]]=nums[G[k][j]]=1
+    istep,jstep=(i//3)*3,(j//3)*3
+    for k in range(istep,istep+3):
+        for l in range(jstep,jstep+3): nums[G[k][l]]=1
+    nums = [i for i,num in enumerate(nums) if num==0 and i>0]
+    for num in nums:
+        G[i][j]=num
+        rec(n+1)
+        G[i][j]=0
+rec(0)
