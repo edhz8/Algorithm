7fe1a9e887eba59a5211a2c84afa119879c90282
GyuhyunBaek <edhz8888@gmail.com>
2023-02-15 11:57:26 +0900

    boj 13335

--- BOJ/13335_트럭.py
+++ BOJ/13335_트럭.py
@@ -1,8 +1,6 @@
-n,w,L = map(int,input().split())
-cars,t,bridge = list(map(int,input().split())),0,[]
-while cars or bridge:
-    t+=1
-    while bridge and bridge[0][0] < 2 : bridge.pop(0)
-    for i in range(len(bridge)): bridge[i][0] -=1
-    if cars and len(bridge)<=w and sum(a for _,a in bridge)+cars[0]<=L: bridge.append([w,cars.pop(0)])
-print(t)
+n,w,L=map(int,input().split())
+T=[*map(int,input().split())]
+B=[0]*w
+A=0
+while sum(B)!=0 or T:B.pop(0);B.append(T.pop(0) if T and sum(B)+T[0]<=L else 0);A+=1
+print(A)
