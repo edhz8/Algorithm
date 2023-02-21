71ac267bf9e75a02785c8c0f9bc2e3c7c19e8144
GyuhyunBaek <edhz8888@gmail.com>
2023-02-21 17:13:51 +0900

    boj 15683

--- BOJ/15683_감시.py
+++ BOJ/15683_감시.py
@@ -1,25 +1,42 @@
 import sys
-I = sys.stdin.readline
-n,m=map(int,I().split())
-graph=[list(map(int,I().split())) for _ in range(n)]
-cctvs=[]
-cctv={1:[(0),(1),(2),(3)] , 2:[(0,2),(1,3)] , 3:[(0,1),(1,2),(2,3),(3,4)] , 4:[(0,1,2),(1,2,3),(2,3,0),(3,1,2)] , 5:[(0,1,2,3)]}
-D=((-1,0),(0,1),(1,0),(0,-1))
-def get_nms(x,y,d):
-    ret = []
-    while True:
-        x,y=x+D[d][0],y+D[d][1]
-        if 0<=x<n and 0<=y<m:
-            if graph[x][y] == 0 : ret.append((x,y))
-            elif graph[x][y] == 6 : break
-        else : break
-    return ret
-for x,y in [(i,j) for i in range(n) for j in range(m) if graph[i][j] in [1,2,3,4,5]]:
-    tmp=[]
-    for d in cctv[graph[x][y]]:
-        t=[]
-        for di in d:
-            t+=get_nms(x,y,di)
-        tmp.append(t)
-    cctvs.append(tmp)
-
+input = sys.stdin.readline
+N,M=map(int,input().split())
+G=[[*map(int,input().split())] for _ in ' '*N]
+D=[(0,-1),(1,0),(0,1),(-1,0)] #북동남서
+cctv={
+    1:[[0],[1],[2],[3]],
+    2:[(0,2),(1,3)],
+    3:[(0,1),(1,2),(2,3),(3,0)],
+    4:[(0,1,2),(1,2,3),(2,3,0),(3,0,1)],
+    5:[(0,1,2,3)]
+}
+points=[]
+answer = N*M
+for x in range(N):
+    for y in range(M):
+        if G[x][y]>0: answer-=1 
+        if 0<G[x][y]<6:
+            ps = []
+            for dx,dy in D:
+                nx,ny=x+dx,y+dy
+                t=set()
+                while 0<=nx<N and 0<=ny<M and 0<=G[nx][ny]<6:
+                    if G[nx][ny]==0 : t.add((nx,ny))
+                    nx+=dx
+                    ny+=dy
+                ps.append(t)
+            nps = []
+            for ways in cctv[G[x][y]]:
+                t=set()
+                for way in ways: t|=ps[way]
+                nps.append(t)
+            points.append(nps)
+L=len(points)
+Max=0
+def rec(index,S):
+    global Max
+    if index==L:
+        Max = max(Max,len(S));return
+    for s in points[index]: rec(index+1,S|s)
+rec(0,set())
+print(answer-Max)
