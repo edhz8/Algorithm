3cd058bbe4806d569501198f558588620fdf3672
GyuhyunBaek <edhz8888@gmail.com>
2023-03-01 20:18:07 +0900

    boj 10836

--- BOJ/10836_여왕벌.py
+++ BOJ/10836_여왕벌.py
@@ -1,9 +1,8 @@
 import sys
 I,R=sys.stdin.readline,range
 M,N=map(int,I().split())
-L=[1]*(2*M+1)
+L=[1]*(2*M-1)
 for _ in ' '*N:
     z,o,t=map(int,I().split())
     for i in R(o+t):L[i+z]+=(1+(i>=o))
-G=[[L[M-1+(j if j else-i)]for j in R(M)]for i in R(M)]
-for i in R(M):print(*G[i])
+for i in R(M):print(L[M-1-i],*L[M:])
