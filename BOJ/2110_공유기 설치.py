6d4c40dad1cb4b96cda39e3f27be539dfca6c179
GyuhyunBaek <edhz8888@gmail.com>
2023-04-12 12:30:51 +0900

    boj 2110

---
+++ BOJ/2110_공유기 설치.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+N,C=map(int,input().split())
+X=sorted([int(input()) for _ in ' '*N])
+Min,Max,answer=1,(X[N-1]-X[0])//(C-1)+1,0
+while Min<=Max:
+    Mid = (Min+Max)//2
+    cnt,cur=1,X[0]
+    for i in range(1,N):
+        if C-cnt>(N-1)-i+1 : break
+        if X[i]-cur >= Mid : cnt+=1;cur=X[i]
+    if cnt>=C : 
+        answer = Mid
+        Min = Mid+1
+    else : Max = Mid-1
+print(answer)
