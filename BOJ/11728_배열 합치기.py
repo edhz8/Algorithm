8932dc83d764a25ead0b580d8890e2372ba95499
GyuhyunBaek <edhz8888@gmail.com>
2023-06-02 12:03:59 +0900

    boj 11728

--- BOJ/11728_배열 합치기.py
+++ BOJ/11728_배열 합치기.py
@@ -1,12 +1,2 @@
-I=lambda:map(int,input().split())
-N,M=I()
-A=sorted(I())
-B=sorted(I())
-i,j=0,0
-answer = []
-while i<N or j<M:
-    if i==N : answer.append(B[j]);j+=1;continue
-    if j==M : answer.append(A[i]);i+=1;continue
-    if B[j]>=A[i] : answer.append(A[i]);i+=1;continue
-    if B[j]<A[i] : answer.append(B[j]);j+=1;continue
-print(*answer)
+n,m,*l=open(0).read().split()
+print(*sorted(l,key=int))
