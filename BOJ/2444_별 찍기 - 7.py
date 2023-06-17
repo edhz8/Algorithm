a70bb4ef27d6cd01dac08dc654f2a70669e756cb
GyuhyunBaek <edhz8888@gmail.com>
2023-06-17 11:50:17 +0900

    boj 2444

--- BOJ/2444_별 찍기 - 7.py
+++ BOJ/2444_별 찍기 - 7.py
@@ -1,4 +1,2 @@
-n=o=int(input())
-while n:print(" "*(n-1)+"*"*((o-n)*2+1));n-=1
-n=o-1
-while n:print(" "*(o-n)+"*"*(n*2-1));n-=1
+o=int(input())
+for n in map(abs,range(1-o,o)):print(" "*(o-n-1)+"*"*(n*2+1))
